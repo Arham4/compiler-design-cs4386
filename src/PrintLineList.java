e48754d54558005e0264da4dc46b69236ca9eb12
Arham J. Siddiqui <arhamjsiddiqui@gmail.com>
2021-11-02 12:57:44 -0500

    part-2: Add Printlinelist nonterminal

--- src/PrintLineList.java
+++ src/PrintLineList.java
@@ -1,6 +1,23 @@
 public final class PrintLineList implements NonTerminalToken {
+    public static PrintLineList withPrintList(PrintList printList) {
+        return new PrintLineList(printList);
+    }
+
+    public static PrintLineList empty() {
+        return new PrintLineList(null);
+    }
+
+    private final PrintList printList;
+
+    private PrintLineList(PrintList printList) {
+        this.printList = printList;
+    }
+
     @Override
     public String asString(int tabs) {
-        return null;
+        if (printList == null) {
+            return "";
+        }
+        return printList.asString(tabs);
     }
 }
