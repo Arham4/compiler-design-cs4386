75fd6626b6ab781224929913ff387286f49ea7ff
Arham J. Siddiqui <arhamjsiddiqui@gmail.com>
2021-11-02 12:31:43 -0500

    part-2: Add IfEnd nonterminal

--- src/IfEnd.java
+++ src/IfEnd.java
@@ -1,6 +1,23 @@
 public final class IfEnd implements NonTerminalToken {
+    public static IfEnd withStmt(Stmt stmt) {
+        return new IfEnd(stmt);
+    }
+
+    public static IfEnd end() {
+        return new IfEnd(null);
+    }
+
+    private final Stmt stmt;
+
+    private IfEnd(Stmt stmt) {
+        this.stmt = stmt;
+    }
+
     @Override
     public String asString(int tabs) {
-        return null;
+        if (stmt == null) {
+            return "";
+        }
+        return "else " + stmt.asString(tabs);
     }
 }
