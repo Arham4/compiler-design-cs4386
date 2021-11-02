8066e3db932f13a28e075c385399b221cf926fe7
Arham J. Siddiqui <arhamjsiddiqui@gmail.com>
2021-11-02 00:06:31 -0500

    part-2: Add Optionalexpr nonterminal

--- src/OptionalExpr.java
+++ src/OptionalExpr.java
@@ -1,4 +1,20 @@
 public final class OptionalExpr implements NonTerminalToken {
+    public static OptionalExpr shownExpr(Expr expr) {
+        return new OptionalExpr(expr, true);
+    }
+
+    public static OptionalExpr hidden() {
+        return new OptionalExpr(null, false);
+    }
+
+    private final Expr expr;
+    private final boolean show;
+
+    private OptionalExpr(Expr expr, boolean show) {
+        this.expr = expr;
+        this.show = show;
+    }
+
     @Override
     public String asString(int tabs) {
         return null;
