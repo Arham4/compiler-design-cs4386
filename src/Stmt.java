197d561098c737b84773ef1ffe5ecaff88dd2c31
Arham J. Siddiqui <arhamjsiddiqui@gmail.com>
2021-11-02 11:40:04 -0500

    part-2: Add Stmts nonterminal

---
+++ src/Stmt.java
@@ -0,0 +1,6 @@
+public final class Stmt implements NonTerminalToken {
+    @Override
+    public String asString(int tabs) {
+        return null;
+    }
+}
--- src/Stmts.java
+++ src/Stmts.java
@@ -1,6 +1,40 @@
 public final class Stmts implements NonTerminalToken {
+    public static final class Builder {
+        private Stmt stmt = null;
+        private Stmts stmts = null;
+
+        public Builder stmt(Stmt stmt) {
+            this.stmt = stmt;
+            return this;
+        }
+
+        public Builder stmts(Stmts stmts) {
+            this.stmts = stmts;
+            return this;
+        }
+
+        public Stmts build() {
+            return new Stmts(stmt, stmts);
+        }
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    private final Stmt stmt;
+    private final Stmts stmts;
+
+    private Stmts(Stmt stmt, Stmts stmts) {
+        this.stmt = stmt;
+        this.stmts = stmts;
+    }
+
     @Override
     public String asString(int tabs) {
-        return null;
+        if (stmt == null || stmts == null) {
+            return "";
+        }
+        return stmt.asString(tabs) + "\n" + stmts.asString(tabs);
     }
 }
