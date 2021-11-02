11d25fe25a97c3e4ccb817e68c18d0240606a632
Arham J. Siddiqui <arhamjsiddiqui@gmail.com>
2021-11-02 11:16:24 -0500

    part-2: Add Argdecls nonterminal

---
+++ src/ArgDeclList.java
@@ -0,0 +1,6 @@
+public final class ArgDeclList implements NonTerminalToken {
+    @Override
+    public String asString(int tabs) {
+        return null;
+    }
+}
--- src/ArgDecls.java
+++ src/ArgDecls.java
@@ -1,6 +1,23 @@
-public class ArgDecls implements NonTerminalToken {
+public final class ArgDecls implements NonTerminalToken {
+    public static ArgDecls of(ArgDeclList argDeclList) {
+        return new ArgDecls(argDeclList);
+    }
+
+    public static ArgDecls empty() {
+        return new ArgDecls(null);
+    }
+
+    private final ArgDeclList argDeclList;
+
+    public ArgDecls(ArgDeclList argDeclList) {
+        this.argDeclList = argDeclList;
+    }
+
     @Override
     public String asString(int tabs) {
-        return null;
+        if (argDeclList == null) {
+            return "";
+        }
+        return argDeclList.asString(tabs);
     }
 }
