c0157cdfd63d29ac1b14290fdaffa28703e45254
Arham J. Siddiqui <arhamjsiddiqui@gmail.com>
2021-11-02 12:13:22 -0500

    part-2: Add arguments to method invocation stmt

---
+++ src/Args.java
@@ -0,0 +1,6 @@
+public final class Args implements NonTerminalToken {
+    @Override
+    public String asString(int tabs) {
+        return null;
+    }
+}
--- src/MethodInvocationStmt.java
+++ src/MethodInvocationStmt.java
@@ -1,16 +1,22 @@
 public final class MethodInvocationStmt implements Stmt {
-    public static MethodInvocationStmt of(String id) {
-        return new MethodInvocationStmt(id);
+    public static MethodInvocationStmt blank(String id) {
+        return new MethodInvocationStmt(id, null);
+    }
+
+    public static MethodInvocationStmt withArgs(String id, Args args) {
+        return new MethodInvocationStmt(id, args);
     }
 
     private final String id;
+    private final Args args;
 
-    private MethodInvocationStmt(String id) {
+    private MethodInvocationStmt(String id, Args args) {
         this.id = id;
+        this.args = args;
     }
 
     @Override
     public String asString(int tabs) {
-        return id + "();";
+        return id + "(" + (args == null ? "" : args.asString(tabs)) + ");";
     }
 }
