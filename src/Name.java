28b39c27c217a787f31928e768f3d73c796519ec
Arham J. Siddiqui <arhamjsiddiqui@gmail.com>
2021-11-02 12:00:49 -0500

    part-2: Add reassign stmt alternation of Stmt nonterminal

---
+++ src/Name.java
@@ -0,0 +1,6 @@
+public final class Name implements NonTerminalToken {
+    @Override
+    public String asString(int tabs) {
+        return null;
+    }
+}
---
+++ src/ReassignStmt.java
@@ -0,0 +1,37 @@
+public final class ReassignStmt implements Stmt {
+    public static class Builder {
+        private Name name;
+        private Expr expr;
+
+        public Builder name(Name name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder expr(Expr expr) {
+            this.expr = expr;
+            return this;
+        }
+
+        public ReassignStmt build() {
+            return new ReassignStmt(name, expr);
+        }
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    private final Name name;
+    private final Expr expr;
+
+    private ReassignStmt(Name name, Expr expr) {
+        this.name = name;
+        this.expr = expr;
+    }
+
+    @Override
+    public String asString(int tabs) {
+        return name.asString(tabs) + " = " + expr.asString(tabs) + ";";
+    }
+}
