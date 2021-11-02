a988abb066590c393303870a46eaeb7f1d0cf2c5
Arham J. Siddiqui <arhamjsiddiqui@gmail.com>
2021-11-02 13:41:02 -0500

    part-2: Add Expr nonterminal

---
+++ src/BinaryOp.java
@@ -0,0 +1,6 @@
+public final class BinaryOp implements NonTerminalToken {
+    @Override
+    public String asString(int tabs) {
+        return null;
+    }
+}
--- src/Expr.java
+++ src/Expr.java
@@ -1,6 +1,57 @@
-public final class Expr implements NonTerminalToken {
-    @Override
-    public String asString(int tabs) {
-        return null;
+public interface Expr extends NonTerminalToken {
+    static Expr simple(Name name) {
+        return name::asString;
+    }
+
+    static Expr simpleInvocation(String id) {
+        return tabs -> id + "()";
+    }
+
+    static Expr invocation(String id, Args args) {
+        return tabs -> id + "(" + args.asString(tabs) + ")";
+    }
+
+    static Expr intlit(int integer) {
+        return tabs -> integer + "";
+    }
+
+    static Expr charlit(char character) {
+        return tabs -> character + "";
+    }
+
+    static Expr strlit(String string) {
+        return tabs -> string;
+    }
+
+    static Expr floatlit(float floatingPoint) {
+        return tabs -> floatingPoint + "";
+    }
+
+    static Expr boollit(boolean value) {
+        return tabs -> value + "";
+    }
+
+    static Expr parenthesized(Expr expr) {
+        return tabs -> "(" + expr.asString(tabs) + ")";
+    }
+
+    static Expr not(Expr expr) {
+        return tabs -> "~" + expr.asString(tabs);
+    }
+
+    static Expr minus(Expr expr) {
+        return tabs -> "-" + expr.asString(tabs);
+    }
+
+    static Expr plus(Expr expr) {
+        return tabs -> "+" + expr.asString(tabs);
+    }
+
+    static Expr binaryOp(Expr expr1, BinaryOp binaryOp, Expr expr2) {
+        return tabs -> expr1.asString(tabs) + " " + binaryOp.asString(tabs) + " " + expr2.asString(tabs);
+    }
+
+    static Expr ternary(Expr expr, Expr exprTrue, Expr exprFalse) {
+        return tabs -> "(" + expr.asString(tabs) + ") ? " + exprTrue.asString(tabs) + " : " + exprFalse.asString(tabs) + ")";
     }
 }
