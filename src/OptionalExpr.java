bba3ff53b1f197e3f233b917b8c3e7e73209a9e1
Arham J. Siddiqui <arhamjsiddiqui@gmail.com>
2021-11-02 22:10:13 -0500

    part-2: Fix OptionalExpr#asString

--- src/OptionalExpr.java
+++ src/OptionalExpr.java
@@ -17,6 +17,9 @@ public final class OptionalExpr implements NonTerminalToken {
 
     @Override
     public String asString(int tabs) {
-        return null;
+        if (!show) {
+            return "";
+        }
+        return expr.asString(tabs);
     }
 }
