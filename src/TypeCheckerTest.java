f1ba7ca95927e9c9895bad958b386fcf52ccfe5e
Arham J. Siddiqui <arhamjsiddiqui@gmail.com>
2021-11-24 14:05:56 -0600

    part-3: Add type checker test driver

---
+++ src/TypeCheckerTest.java
@@ -0,0 +1,48 @@
+import java.io.*;
+
+import java_cup.runtime.*;
+import tokens.Program;
+import type_checking.TypeCheckException;
+
+public class TypeCheckerTest {
+
+    public static void main(String[] args) throws Exception {
+        Reader reader = null;
+
+        if (args.length == 1) {
+            File input = new File(args[0]);
+            if (!input.canRead()) {
+                System.out.println("Error: could not read [" + input + "]");
+            }
+            reader = new FileReader(input);
+        } else {
+            reader = new InputStreamReader(System.in);
+        }
+
+        Grammar scanner = new Grammar(reader);   // create scanner
+
+        parser parser = new parser(scanner); // create parser
+        Program program = null;
+
+        try {
+            program = (Program) parser.parse().value;
+            //program = (Program) parser.debug_parse().value;  // parse
+            //The above line of code will output current state and what token is being processed
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.print(program.asString(0));
+        //Now test type checking
+        try {
+            program.typeCheck();
+            System.out.println("Type checking complete!");
+        } catch (TypeCheckException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void error(String s) {
+        System.out.println(s);
+    }
+}
