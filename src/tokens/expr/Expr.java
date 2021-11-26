edbe4c26365a2df0b38e463e59e986fe4bde089a
Arham J. Siddiqui <arhamjsiddiqui@gmail.com>
2021-11-26 13:35:49 -0600

    part-3: Add type checking to floatlit expr

--- src/tokens/expr/Expr.java
+++ src/tokens/expr/Expr.java
@@ -119,7 +119,7 @@ public interface Expr extends NonTerminalToken, TypeCheckable<Type> {
 
             @Override
             public Type typeCheck(int scope, Map<String, Map<Integer, Type>> variableSymbolTable, Map<String, Type> methodSymbolTable) throws TypeCheckException {
-                return null;
+                return Types.FLOATLIT;
             }
         };
     }
